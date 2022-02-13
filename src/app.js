93109797a4e80f5fbaca5218e190fef710e9383e
ciaranbrady555 <ciaranbrady1060@gmail.com>
2022-02-13 00:49:52 +0000

    some stuff

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const APIcontrol = (function() {
+
+    const clientId= '';
+    const clientSecret='';
+
+    const _getToken = async () -> {
+        const result = await fetch(''), {
+            method: 'POST',
+            headers:{
+                'Content-Type' : '',
+                
+            }
+        }
+    }
+})();
